971a7abd1e32a5412ee4e3c1645d9c0bc827527c
muhamedyousef <mohhamedyousef99@gmail.com>
2021-06-29 04:37:54 +0200

    add tailwindCss

--- commentsOnlyJs/server.js
+++ commentsOnlyJs/server.js
@@ -1,21 +1,34 @@
 // Setup empty JS object to act as endpoint for all routes
+var express = require('express');
+var cors = require('cors');
+var app = express();
+let port = 5500;
+
 // Express to run server and routes
 
 // Start up an instance of app
 
 /* Dependencies */
 /* Middleware*/
-
+app.use(cors());
 //Here we are configuring express to use body-parser as middle-ware.
 // Cors for cross origin allowance
-
+app.get('/', (req, res, next) => {
+    res.send('hello world');
+});
+app.get('website/index.html/hello', (req, res, next) => {
+    res.send('hello world');
+});
 // Initialize the main project folder
-
-// Spin up the server
-// Callback to debug
+app.use(express.static('website'))
+    // Spin up the server
+    // Callback to debug
 
 // Initialize all route with a callback function
 
 // Callback function to complete GET '/all'
 
 // Post Route
+app.listen(port, () => {
+    console.log(`Example app is listening at url http://localhost:${port}`);
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,8 @@
 /* Global Variables */
+let date = document.querySelector("#date");
+let temp = document.querySelector("#temp");
+let content = document.querySelector("#content");
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
